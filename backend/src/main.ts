804ae08f36afcb89a658712b32a3ff4a7a4f3417
ggalmury <body0922@naver.com>
2025-09-08 20:26:36 +0900

    feat(backend): 환경변수 및 SQLite 데이터베이스 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { initializeTransactionalContext, StorageDriver } from "typeorm-transactional";
+
+import AppModule from "@/app.module";
 
 async function bootstrap() {
+  initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("api");
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
