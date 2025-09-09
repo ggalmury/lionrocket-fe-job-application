4ed9a08fe7c5455f227affcd8eac0fa890e4743d
ggalmury <body0922@naver.com>
2025-09-09 19:12:14 +0900

    feat(frontend): 타이포그래피 컴포넌트 구현

---
+++ frontend/src/components/atoms/typography/props/typographyProps.ts
@@ -0,0 +1,6 @@
+import type { TypographyStyleOptions } from "@/components/atoms/typography/options/typographyStyleOptions";
+
+export interface TypographyProps {
+  text: string;
+  styles?: TypographyStyleOptions;
+}
